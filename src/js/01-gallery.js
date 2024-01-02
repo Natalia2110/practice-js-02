369e7e77b163fd17bf4f02e498f2070e38440636
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-01-02 20:26:29 +0200

    task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulEl = document.querySelector('.gallery');
+console.log(ulEl);
+
+const createLiEl = galleryItems.reduce((murkup, { description, original, preview }) => 
+  murkup + 
+        `<li class="gallery__item">
+            <a class="gallery__link" href=${original}>
+                <img
+                    class="gallery__image"
+                    src=${preview}
+                    data-source=${original}
+                    alt="${description}"
+                />
+            </a>
+        </li>`, "");
+
+ulEl.insertAdjacentHTML('afterbegin', createLiEl);
+
+function handleClickOnImg(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+}
+
+ulEl.addEventListener('click', handleClickOnImg);
+

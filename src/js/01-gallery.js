d4d6ac89e643a01f3f58b3345dfc3c8ab2ffdba8
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-01-08 18:03:12 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,13 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const ulEl = document.querySelector('.gallery');
-console.log(ulEl);
+// const modalEl = document.querySelector('.modal');
+// console.log(ulEl);
+// console.log(modalEl);
 
 const createLiEl = galleryItems.reduce((murkup, { description, original, preview }) => 
   murkup + 
@@ -21,11 +24,37 @@ const createLiEl = galleryItems.reduce((murkup, { description, original, preview
 
 ulEl.insertAdjacentHTML('afterbegin', createLiEl);
 
+
+
 function handleClickOnImg(event) {
-    if (event.target.nodeName !== "IMG") {
-        return;
-      }
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;       
+      } else {
+        const selectImg = event.target.dataset.source;
+
+        // console.log(selectImg);
+
+        const instance = basicLightbox.create(`
+            <div class="modal">
+                <img src=${selectImg} width="800" height="600">
+            </div>
+        `)
+
+        instance.show();
+        
+      }     
 }
+
+// function closeModal(){
+            
+//     const visible = basicLightbox.visible()
+//     if(visible) {
+        
+//         instance.close();
+//     }
+
+// modalEl.addEventListener('keydown',  closeModal);
 
 ulEl.addEventListener('click', handleClickOnImg);
 
